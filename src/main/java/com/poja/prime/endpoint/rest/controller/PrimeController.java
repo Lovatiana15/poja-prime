9d4336b4d75c55821813914f4fd8696e51452d2e
Lovatiana15 <hei.lovatiana@gmail.com>
2023-12-12 20:24:05 +0200

    fix: bug annotation

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -3,6 +3,8 @@ import com.poja.prime.service.prime.PrimeService;
 import java.math.BigInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.math.BigInteger;
